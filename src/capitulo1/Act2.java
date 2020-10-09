b48593ca60e299200791fb8f6ab75810ceab86df
Eli <eliseasj@gmail.com>
2020-10-09 19:47:17 +0200

    09/10 actividades hasta la 03 del bloque 1 cap2

--- src/capitulo1/Act2.java
+++ src/capitulo1/Act2.java
@@ -9,27 +9,28 @@ public class Act2 {
 
 //Realiza un programa igual al anterior, con la particularidad de que ahora 
 //se debe mostrar cada número justo a continuación del momento en que ha sido introducido.
-	
-	//Declaro las variables
-		
+
+		// Declaro las variables
+
 		String srt;
 		int numentero;
 		float numflotante;
-		double numdoble; 
-		
-		//Pido 3 números con srt = JOptionPane.showInputDialog y los guardo en las variables. Después los muestro.
-		
+		double numdoble;
+
+		// Pido 3 números con srt = JOptionPane.showInputDialog y los guardo en las
+		// variables. Después los muestro.
+
 		srt = JOptionPane.showInputDialog("Introduzca un número entero: ");
 		numentero = Integer.parseInt(srt);
 		System.out.println("El número entero es: " + numentero);
-		
+
 		srt = JOptionPane.showInputDialog("Introduzca un número flotante: ");
 		numflotante = Float.parseFloat(srt);
 		System.out.println("El número flotante es: " + numflotante);
-		
+
 		srt = JOptionPane.showInputDialog("Introduzca un número doble: ");
 		numdoble = Double.parseDouble(srt);
 		System.out.println("El número doble es: " + numdoble);
 
-}
+	}
 }
