f79d4df4251e018faa982c560684934c5431c3d7
Eli <eliseasj@gmail.com>
2020-10-16 13:53:32 +0200

    bloque3 acabado

--- src/capitulo2/bloque3/Act2.java
+++ src/capitulo2/bloque3/Act2.java
@@ -1,11 +1,34 @@
 package capitulo2.bloque3;
 
+import javax.swing.JOptionPane;
+
 public class Act2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 /* Realiza una calculadora avanzada, pero con la particularidad de que
 las operaciones deben ser raices, potencias y el módulo de la división.*/
+	
+	String srt;
+	double indice, exponente, num, restodiv, divisor, raiz, pot;
+	
+	srt = JOptionPane.showInputDialog("Introduce un número para realizar la raíz, la potencia y el resto de la división. Número: ");
+	num = Double.parseDouble(srt);
+	
+	srt = JOptionPane.showInputDialog("Introduce el índice de la raíz: ");
+	indice = Double.parseDouble(srt);
+	
+	srt = JOptionPane.showInputDialog("Introduce el exponente de la potencia: ");
+	exponente = Double.parseDouble(srt);
+	
+	srt = JOptionPane.showInputDialog("Introduce un divisor para realizar la división: ");
+	divisor = Double.parseDouble(srt);
+	
+	raiz = Math.pow(num, 1/indice);
+	pot = Math.pow(num, exponente);
+	restodiv = num % divisor;
+	
+	System.out.println("El resultado de la raíz es: "+ raiz + "\nEl resultado de la potencia es: " + pot + "\nEl resto de la división es: " + restodiv);
 	}
 
 }
--- src/capitulo2/bloque3/Act4.java
+++ src/capitulo2/bloque3/Act4.java
@@ -1,5 +1,7 @@
 package capitulo2.bloque3;
 
+import javax.swing.JOptionPane;
+
 public class Act4 {
 
 	public static void main(String[] args) {
@@ -13,7 +15,55 @@ Ejemplo: Pagamos con 2000 unidades monetarias un articulo que nos cuesta 375 uni
 - 1 moneda de 25 = 25 */
 
 	
+	String srt;
+	int importe, entrega, cambio;
+	int coci100 = 0, coci50 = 0, coci25 = 0, coci5 = 0, coci1 = 0, suma100, suma50, suma25, suma5, suma1;
 	
+	srt = JOptionPane.showInputDialog("Introduce el importe de la compra: ");
+	importe = Integer.parseInt(srt);
+	
+	srt = JOptionPane.showInputDialog("Introduce el dinero dado para pagar: ");
+	entrega = Integer.parseInt(srt);
+	
+	cambio = entrega - importe;
+	
+	if (cambio > 0) {
+		if ((cambio / 100) > 0 ) {
+			coci100 = cambio / 100; //para saber cuantas monedas de 100 necesito
+			suma100 = coci100 * 100; //para saber el total de las monedas de 100
+			
+			System.out.println(coci100 + " monedas de 100: " + suma100);
+			cambio = importe - suma100;
+		}
+		if ((cambio / 50) > 0) {
+			coci50 = cambio / 50;
+			suma50 = coci50 * 50;
+			
+			System.out.println(coci50 + " monedas de 50: " + suma50);
+			cambio = cambio - suma50;
+		}
+		if ((cambio / 25) > 0) {
+			coci25 = cambio / 25;
+			suma25 = coci25 * 25;
+			
+			System.out.println(coci25 + " monedas de 25: " + suma25);
+			cambio = cambio - suma25;
+		}
+		if ((cambio / 5) > 0) {
+			coci5 = cambio / 5;
+			suma5 = coci5 * 5;
+			
+			System.out.println(coci5 + " monedas de 5: " + suma5);
+			cambio = cambio - suma5;
+		}
+		if ((cambio / 1) > 0) {
+			coci1 = cambio / 1;
+			suma1 = coci1 * 1;
+			
+			System.out.println(coci1 + " monedas de 1: " + suma1);
+		}
+	}
+	else System.out.println("No hay cambio.");
 	}
 
 }
